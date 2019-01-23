ce52a176f16b7af8bef3ac74e8091e801d3f9dc3
李顺仪 <lishunyi0109@gmail.com>
2019-01-23 18:19:10 +0800

    添加git配置信息

--- src/main/java/com/lishunyi/configserver/ConfigServerApplication.java
+++ src/main/java/com/lishunyi/configserver/ConfigServerApplication.java
@@ -5,8 +5,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+/**
+ * @ClassName: ConfigServerApplication
+ * @Description: 项目启动类
+ * @Author: 李顺仪
+ * @Date: 2019/1/23 14:54
+ * @Version: 1.0
+ */
 @SpringBootApplication
-//@EnableConfigServer
+@EnableConfigServer
 @EnableEurekaClient
 public class ConfigServerApplication {
 
